5ba3f500602fa78937c547631ca3808398d969db
Jonathan Glassey <jono@shuttlerock.com>
2018-07-22 01:18:38 +0900

    Set club of schedule in instance method

--- lib/central_sports/client.rb
+++ lib/central_sports/client.rb
@@ -2,25 +2,25 @@ class CentralSports::Client
   include HTTParty
   base_uri 'https://www.central.co.jp'
 
-  def initialize(club_id, date=Time.now.strftime('%Y%m'))
-    @options = {
-      format: :plain,
-      query: {
-        club_code: club_id,
-        yyyymm:    date
-      }
-    }
-  end
-
-  def schedule
+  def schedule_for_club(club_id, date=Time.now.strftime('%Y%m'))
     return @schedule if defined?(@schedule)
-    response = self.class.get('/club/jsonp_schedule.php', @options)
+    response = self.class.get('/club/jsonp_schedule.php', schedule_options(club_id, date))
     # The Central Sports API returns () brackets at each end of the response. These cause an error
     # when encoding the JSON to Hash so we want to remove them.
 
     data = Oj.load(response.gsub('(', '').gsub(')', '').gsub('callback', ''))
     raise ClubDoesNotExistError.new if data['status'] == false
     @schedule = CentralSports::Schedule.new(data)
+  end
+
+  def schedule_options(club_id, date)
+    {
+      format: :plain,
+      query: {
+        club_code: club_id,
+        yyyymm:    date
+      }
+    }
   end
 
   class ClubDoesNotExistError < StandardError; end
