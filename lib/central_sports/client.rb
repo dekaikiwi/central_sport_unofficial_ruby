8f6738523b26dd9edb42f711f5d49336381387c3
Jonathan Glassey <jono@shuttlerock.com>
2018-07-22 01:46:38 +0900

    Briefly de-memoize schedule getter
    It wont work with currnt implementatoin as it has paramters

--- lib/central_sports/client.rb
+++ lib/central_sports/client.rb
@@ -3,14 +3,13 @@ class CentralSports::Client
   base_uri 'https://www.central.co.jp'
 
   def schedule_for_club(club_id, date=Time.now.strftime('%Y%m'))
-    return @schedule if defined?(@schedule)
     response = self.class.get('/club/jsonp_schedule.php', schedule_options(club_id, date))
     # The Central Sports API returns () brackets at each end of the response. These cause an error
     # when encoding the JSON to Hash so we want to remove them.
 
     data = Oj.load(response.gsub('(', '').gsub(')', '').gsub('callback', ''))
     raise ClubDoesNotExistError.new if data['status'] == false
-    @schedule = CentralSports::Schedule.new(data)
+    CentralSports::Schedule.new(data)
   end
 
   def schedule_options(club_id, date)
