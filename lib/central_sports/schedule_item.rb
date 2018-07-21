bbd9112ecae30d5a656df1360869f8dc58a99cd8
Jonathan Glassey <jono@shuttlerock.com>
2018-07-22 01:19:18 +0900

    Calculate start time from API response.

--- lib/central_sports/schedule_item.rb
+++ lib/central_sports/schedule_item.rb
@@ -20,12 +20,19 @@ class CentralSports::ScheduleItem
   attr_reader :name, :location, :instructor_name, :school, :for_kids
 
   def initialize(data)
+    @data = data
     @name = data[:name]
     @location = data[:location]
     @instructor_name = data[:instructor_name]
     @school = data[:is_school]
     @for_kids = data[:is_for_kids]
     @day_of_week = DAYS_OF_THE_WEEK[@data[:day_of_week_no].to_i]
+  end
+
+  def start_time
+    time = @data[:start_time]
+    time = time.length == 4 ? time : "0#{time}"
+    Time.strptime(time, '%H%M')
   end
 
   ITEM_TYPES.each do |key, value|
