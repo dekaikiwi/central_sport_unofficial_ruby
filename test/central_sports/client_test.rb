f401890643c3387dae054405b27656a8c5334896
Jonathan Glassey <jono@shuttlerock.com>
2018-07-22 01:18:46 +0900

    Update specs

--- test/central_sports/client_test.rb
+++ test/central_sports/client_test.rb
@@ -5,10 +5,19 @@ class CentralSportsClientTest < Minitest::Test
     assert ::CentralSports::Client
   end
 
-  def test_schedule
+  def test_schedule_for_club
     VCR.use_cassette('schedule', record: :new_episodes) do
-      client = CentralSports::Client.new(145)
-      client.schedule
+      client = CentralSports::Client.new
+      client.schedule_for_club(145)
+    end
+  end
+
+  def test_schedule_raise_error_when_store_doesnt_exist
+    VCR.use_cassette('schedule', record: :new_episodes) do
+      assert_raises ::CentralSports::Client::ClubDoesNotExistError do
+        client = CentralSports::Client.new
+        client.schedule_for_club(99999)
+      end
     end
   end
 end
